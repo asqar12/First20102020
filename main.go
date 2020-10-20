4ad38b7abb24d4dee6ff8073a0144bc37c10b855
asqar12 <easqar12@gmail.com>
2020-10-21 01:19:03 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+import (
+	"fmt"
+)
+func main (){
+	fmt.Println("     *")
+	fmt.Println("   * * *")
+	fmt.Println("     *")
+	fmt.Println("   * * *")
+	fmt.Println("  * * * * ")
+	fmt.Println("     *")
+	fmt.Println("  * * * * ")
+	fmt.Println(" * * * * *")
+	fmt.Println("* * * * * * ")
+	fmt.Println("     *")
+	fmt.Println("     *")
+}
